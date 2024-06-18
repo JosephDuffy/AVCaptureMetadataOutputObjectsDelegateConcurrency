8ba10b848d74ea5da307d05aa379a35e34dae77c
Joseph Duffy <commits@josephduffy.co.uk>
2024-06-17 17:33:10 -0700

    Add example AVCaptureMetadataOutputObjectsDelegate under Swift 6

--- AVCaptureMetadataOutputObjectsDelegateConcurrency/ViewController.swift
+++ AVCaptureMetadataOutputObjectsDelegateConcurrency/ViewController.swift
@@ -1,19 +1,28 @@
-//
-//  ViewController.swift
-//  AVCaptureMetadataOutputObjectsDelegateConcurrency
-//
-//  Created by Joseph Duffy on 17/06/2024.
-//
-
+import AVFoundation
 import UIKit
 
-class ViewController: UIViewController {
+@MainActor
+final class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    private var output: AVCaptureMetadataOutput?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: .main)
+        self.output = output
     }
 
+    private func doSomethingWithMetadataObjects(_ metadataObjects: [AVMetadataObject]) {}
 
+    // Must be marked nonisolated because the AVCaptureMetadataOutputObjectsDelegate protocol cannot declare the actor on which the function will be called.
+    nonisolated func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // We know it's on the main actor so we should be able to assume
+        // isolated and pass metadata objects to another function.
+        MainActor.assumeIsolated {
+            // This does not compile though, presumably because AVMetadataObject
+            // is not Sendable.
+            doSomethingWithMetadataObjects(metadataObjects) // Sending 'metadataObjects' risks causing data races
+        }
+    }
 }
-
